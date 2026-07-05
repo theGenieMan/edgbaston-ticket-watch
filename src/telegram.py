16b5adf5a9a8488d15364077847e4242d452180b
theGenieMan <noirjambon@gmail.com>
2026-07-05 10:38:43 +0100

    Update telegram.py

--- src/telegram.py
+++ src/telegram.py
@@ -1,1 +1,27 @@
+import os
+import requests
 
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_message(message: str):
+
+    if not BOT_TOKEN or not CHAT_ID:
+        raise RuntimeError("Telegram credentials missing")
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    response = requests.post(
+        url,
+        data={
+            "chat_id": CHAT_ID,
+            "text": message,
+        },
+        timeout=20,
+    )
+
+    response.raise_for_status()
+
+    return response
