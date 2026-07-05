23061c445782d1d0fe77adedad3f4d545aac0648
theGenieMan <noirjambon@gmail.com>
2026-07-05 10:32:31 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,18 @@
+"""
+Edgbaston Ticket Watch
+Configuration
+"""
 
+EVENT_NAME = "2026 England vs India Men's ODI"
+
+URL = "https://ticketexchange.edgbaston.com/list/resaleProducts?lang=en"
+
+TIMEZONE = "Europe/London"
+
+CHECK_START_HOUR = 7
+CHECK_END_HOUR = 22
+
+BUYING_TICKETS = 1
+
+TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
+TELEGRAM_CHAT_ID = "TELEGRAM_CHAT_ID"
