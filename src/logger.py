67db19ebc9d9979f2b629aeb38348647d6d6cf03
theGenieMan <noirjambon@gmail.com>
2026-07-05 11:30:52 +0100

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,13 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+from config import TIMEZONE
+
+
+def log(message):
+
+    now = datetime.now(ZoneInfo(TIMEZONE))
+
+    print(
+        f"[{now.strftime('%Y-%m-%d %H:%M:%S')}] {message}"
+    )
